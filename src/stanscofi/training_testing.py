2d19de1c24bfed25d02bacc0a98db4f5994fdb6b
REDA Clemence <clemence.reda@inria.fr>
2023-06-09 14:54:00 +0200

    <tests> stanscofi.training_testing: print out

--- src/stanscofi/training_testing.py
+++ src/stanscofi/training_testing.py
@@ -108,8 +108,8 @@ def traintest_validation_split(dataset, test_size, early_stop=None, metric="cosi
         clusters = fcluster(Z, nc, criterion='maxclust', depth=2, R=None, monocrit=None)
         print(ratings)
         print(clusters)
-        print(ratings[np.vectorize(lambda x : clusters[x]<=c)(ratings[:,1]))
-        nratings_train = {ratings[np.vectorize(lambda x : clusters[x]<=c)(ratings[:,1]).astype(bool),:].shape[0]:c for c in range(1,len(np.unique(clusters))+1)}
+        print(ratings[np.vectorize(lambda x : clusters[x]<=c)(ratings[:,1]),:])
+        nratings_train = {ratings[np.vectorize(lambda x : clusters[x]<=c)(ratings[:,1]),:].shape[0]:c for c in range(1,len(np.unique(clusters))+1)}
         select_clust = np.max([k if (k<=train_nset) else -1 for k in nratings_train])
         cluster_size = nratings_train.get(select_clust, -1)
         if (verbose):
