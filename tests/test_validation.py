0b17753db49d02014eff725f39ac8eda4a7359c2
REDA Clemence <clemence.reda@inria.fr>
2023-06-09 10:13:35 +0200

    <docs> add info on predict, <tests> stanscofi.validation: fix imports

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 
+import stanscofi.datasets
 import stanscofi.validation
 
 class TestValidation(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestValidation(unittest.TestCase):
     def generate_dataset_scores_threshold(self):
         threshold=0.5
         npositive, nnegative, nfeatures, mean, std = 200, 100, 50, 0.5, 1
-        data_args = generate_dummy_dataset(npositive, nnegative, nfeatures, mean, std)
+        data_args = stanscofi.datasets.generate_dummy_dataset(npositive, nnegative, nfeatures, mean, std)
         dataset = stanscofi.datasets.Dataset(**data_args)
         ## Generate random class scores
         pi=1/16
@@ -26,7 +27,7 @@ class TestValidation(unittest.TestCase):
         dataset, scores, threshold = self.generate_dataset_scores_threshold()
         predictions = np.copy(scores)
         predictions[:,2] = (-1)**(predictions[:,2]<threshold)
-        metrics, _ = compute_metrics(scores, predictions, dataset, beta=1, ignore_zeroes=False, verbose=False)
+        metrics, _ = stanscofi.validation.compute_metrics(scores, predictions, dataset, beta=1, ignore_zeroes=False, verbose=False)
         print(metrics)
         self.assertEqual(metrics.shape[0], 2)
         self.assertEqual(metrics.shape[1], 2)
@@ -35,7 +36,7 @@ class TestValidation(unittest.TestCase):
         dataset, scores, threshold = self.generate_dataset_scores_threshold()
         predictions = np.copy(scores)
         predictions[:,2] = (-1)**(predictions[:,2]<threshold)
-        _, plot_args = compute_metrics(scores, predictions, dataset, beta=1, ignore_zeroes=False, verbose=False)
+        _, plot_args = stanscofi.validation.compute_metrics(scores, predictions, dataset, beta=1, ignore_zeroes=False, verbose=False)
         stanscofi.validation.plot_metrics(**plot_args, figsize=(10,10), model_name="Random on Dummy")
         ## if it ends without any error, it is a success
 
