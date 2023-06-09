6216dbdbec68584ad60d7ad4abd53f161a8eb2cb
REDA Clemence <clemence.reda@inria.fr>
2023-06-09 10:59:59 +0200

    <tests> stanscofi.models: upload test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,22 +1,52 @@
 import unittest
 import pandas as pd
-import numpy as np
+
+import stanscofi.datasets
 import stanscofi.models
 
-def fold2mask(fold):
-    return pd.DataFrame(fold, columns=list('ijv')).pivot(
-        index='i', columns='j')
+class TestModels(unittest.TestCase):
 
-class TestFolds(unittest.TestCase):
+    ## Generate example
+    def generate_dataset(self):
+        npositive, nnegative, nfeatures, mean, std = 20, 10, 10, 0.5, 1
+        data_args = stanscofi.datasets.generate_dummy_dataset(npositive, nnegative, nfeatures, mean, std)
+        dataset = stanscofi.datasets.Dataset(**data_args)
+        return dataset
 
-    def test_get_folds(self):
-        df = pd.DataFrame(np.random.randint(2, size=(6, 5)))
-        print(df)
-        # folds = get_folds(df)
-        folds = get_folds_disjoint_diseases(df)
-        train, test = folds[0]
-        print(train)
-        print('train', fold2mask(train))
-        print('test', fold2mask(test))
-        print(len(train), len(test))
-        self.assertEqual('hello', 'hello')
+    def test_scores2ratings(self):
+        dataset = self.generate_dataset()
+        preds = np.random.normal(0,1,size=np.prod(dataset.ratings_mat.shape)).reshape(dataset.ratings_mat.shape)
+        ## matrix form
+        scores = pd.DataFrame(preds, index=dataset.item_list, columns=dataset.user_list)
+        ratings = stanscofi.models.scores2ratings(scores)
+        self.assertEqual(ratings.shape[1], 3)
+        self.assertEqual(ratings.shape[0], np.prod(scores.shape))
+        self.assertEqual(ratings[0,2], scores[0,0])
+        self.assertEqual(ratings[-1,2], scores[-1,-1])
+
+    def test_NMF(self):
+        dataset = self.generate_dataset()
+        params = {"init":None, "solver":'cd', "beta_loss":'frobenius', "tol":0.0001, "max_iter":100, 
+          "random_state":12345, "alpha_W":0.0, "alpha_H":'same', "l1_ratio":0.0, "verbose":0, 
+          "shuffle":False, "n_components": np.min(dataset.ratings_mat.shape)//2+1, "decision_threshold": 0.005}
+        model = stanscofi.models.NMF(params)
+        model.fit(dataset)
+        scores = model.predict(dataset)
+        predictions = model.classify(scores)
+        ## if it ends without any error, it is a success
+
+    def test_LogisticRegression(self):
+        dataset = self.generate_dataset()
+        params = {"penalty":'elasticnet', "C":1.0, "fit_intercept":True, "class_weight":"balanced", 
+          "intercept_scaling":1., "random_state":12345, "max_iter":100, "tol": 1e-4, 
+          "multi_class":'multinomial', "n_jobs": 1, "l1_ratio":1, "solver": "saga", 
+          ## parameter subset allows to only consider Top-N features in terms of cross-sample variance for speed-up 
+          "preprocessing": "same_feature_preprocessing", "subset": 5, "decision_threshold": 0.75}
+        model = stanscofi.models.LogisticRegression(params)
+        model.fit(dataset)
+        scores = model.predict(dataset)
+        predictions = model.classify(scores)
+        ## if it ends without any error, it is a success
+
+if __name__ == '__main__':
+    unittest.main()
